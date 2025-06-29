a41aa92d0009fb65c97d148fa92a665e2d4a518b
Habosa162 <habosa2000@hotmail.com>
2025-06-29 20:33:08 +0300

    Project Structure

---
+++ API/Program.cs
@@ -0,0 +1,105 @@
+using Amazon.Runtime;
+using Amazon.S3;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using RetailEcommerce.Domain.Models;
+using RetailEcommerce.Infrastructure.Data;
+using Scalar.AspNetCore;
+using System.Text;
+
+namespace RetailEcommerce.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddControllers();
+
+            //CORS Configuration
+            builder.Services.AddCors(options =>
+            {
+                //https://legacy-estates.co
+                options.AddPolicy("AllowAngularApp", policy =>
+                {
+                    policy.WithOrigins("http://localhost:4200") // Explicitly allow Angular frontend
+                          .AllowAnyMethod()                    // Allow GET, POST, OPTIONS, etc.
+                          .AllowAnyHeader()                    // Allow Authorization, Content-Type, etc.
+                          .AllowCredentials();                 // Support credentialed requests
+                });
+            });
+            
+            
+            //add dbcontext
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            
+            
+            
+            //add identity
+            builder.Services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            
+            
+            
+            // Configure Authentication using JSON web Token
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+
+
+
+            //AWS S3 Service
+            builder.Services.AddSingleton<AWSCredentials>(sp =>
+                new BasicAWSCredentials(
+                    builder.Configuration["AWS:AccessKey"],
+                    builder.Configuration["AWS:SecretKey"]
+                )
+            );
+            builder.Services.AddSingleton<IAmazonS3>(sp =>
+                new AmazonS3Client(
+                    sp.GetRequiredService<AWSCredentials>(),
+                    Amazon.RegionEndpoint.GetBySystemName(builder.Configuration["AWS:Region"])
+                )
+            );
+            
+            
+            
+            //Projects Injection
+
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapScalarApiReference();
+            }
+            app.UseRouting();
+            app.UseCors("AllowAngularApp");
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RetailEcommerce.Domain.Models;
+
+namespace RetailEcommerce.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>().HasData(
+              new IdentityRole { Id = "1", ConcurrencyStamp = "1", Name = "Master", NormalizedName = "MASTER" },
+              new IdentityRole { Id = "2", ConcurrencyStamp = "2", Name = "Admin", NormalizedName = "ADMIN" },
+              new IdentityRole { Id = "3", ConcurrencyStamp = "3", Name = "Client", NormalizedName = "GYMOWNER" }
+            );
+        }
+    }
+}
